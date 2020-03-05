1ad2c66a48656caf448dead5f8da95fdfd01cdf1
Bartusiak <bsk69@o2.pl>
2020-03-05 07:28:25 +0100

    Added Handler().postDelayed to SplashActivity

--- app/src/main/java/dog/snow/androidrecruittest/SplashActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/SplashActivity.kt
@@ -1,9 +1,15 @@
 package dog.snow.androidrecruittest
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
+    private val SPLASH_TIME_OUT = 1000;
+
     private fun showError(errorMessage: String?) {
         MaterialAlertDialogBuilder(this)
             .setTitle(R.string.cant_download_dialog_title)
@@ -14,4 +20,17 @@ class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
             .apply { setCanceledOnTouchOutside(false) }
             .show()
     }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState);
+        Handler().postDelayed(object:Runnable{
+            public override fun run(){
+                val intent = Intent(this@SplashActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        },SPLASH_TIME_OUT.toLong())
+    }
+
+
 }
